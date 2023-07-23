52978fc205cf007b334fc021c1984e7f36ff4a5e
Applesry <129361139+Applestry@users.noreply.github.com>
2023-07-23 19:51:28 +0700

    Update Algorithm.h

--- Lab3/Algorithm.h
+++ Lab3/Algorithm.h
@@ -5,3 +5,8 @@ void mergeSort(int a[], int left, int right, int& count_compare);
 void shakerSort(int a[], int n, int& count_compare);
 void count_radixSort(int a[], int n, int d, int& count_compare);
 void radixSort(int a[], int n, int& count_compare);
+void bubbleSort(int a[], int n, int& comparisons);
+void flashSort(int a[], int n, int& comparisons);
+void heapRebuild(int a[], int pos, int n, int& comparisons);
+void heapConstruct(int a[], int n, int& comparisons);
+void heapSort(int a[], int n, int& comparisons);
