5cc2e512b82ef104409701e6d716116e708e0d4a
Le Hanh <128907951+Lehhanh@users.noreply.github.com>
2023-07-26 20:08:47 +0700

    Update Main.cpp

--- Lab3/Main.cpp
+++ Lab3/Main.cpp
@@ -2,16 +2,17 @@
 #include "DataGenerator.h"
 #include "Command.h"
 using namespace std;
-void printArray(int a[], int n) {
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << " ";
-	}
-	cout << endl;
-}
+//void printArray(int a[], int n) {
+//	for (int i = 0; i < n; i++) {
+//		cout << a[i] << " ";
+//	}
+//	cout << endl;
+//}
 
 int main(int argc, char* argv[]) {
-	string mode = argv[1];
-	if (mode == "-a") {
+	if (argc < 2)
+		return 0;
+	if (strcmp(argv[1], "-a") == 0) {
 		if (argc == 5) {
 			//command 1 or 3
 		}
@@ -23,12 +24,13 @@ int main(int argc, char* argv[]) {
 			cout << "invalid command line argument" << endl;
 		}
 	}
-	else if (mode == "-c") {
+	else if (strcmp(argv[1], "-c") == 0) {
 		if (argc == 5) {
 			//command 4
 		}
 		else if (argc == 6) {
 			//command 5
+			Command_5(argc, argv);
 		}
 		else {
 			cout << "invalid command line argument" << endl;
