3401d1827b574d93757434ab56fab9eae6c20a10
TuMinz <129361284+Tuminz@users.noreply.github.com>
2023-07-26 21:35:02 +0700

    Update Main.cpp

--- Lab3/Main.cpp
+++ Lab3/Main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char* argv[]) {
 	if (strcmp(argv[1], "-a") == 0) {
 		if (argc == 5) {
 			//command 1 or 3
+			Command_1(argc, argv);
 		}
 		else if (argc == 6) {
 			//command 2
@@ -27,6 +28,7 @@ int main(int argc, char* argv[]) {
 	else if (strcmp(argv[1], "-c") == 0) {
 		if (argc == 5) {
 			//command 4
+			Command_4(argc, argv);
 		}
 		else if (argc == 6) {
 			//command 5
