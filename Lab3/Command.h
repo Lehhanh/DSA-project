05ce61914eb1846f2efa3241326dd901c267e5de
Applesry <129361139+Applestry@users.noreply.github.com>
2023-07-26 23:18:12 +0700

    Update Command.h

--- Lab3/Command.h
+++ Lab3/Command.h
@@ -8,5 +8,6 @@
 
 void Command_1(int argc, char* argv[]);
 void Command_2(int argc, char* argv[]);
+void Command_3(int argc, char* argv[]);
 void Command_4(int argc, char* argv[]);
 void Command_5(int argc, char* argv[]);
