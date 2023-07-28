083538fe379ca4306b8f1f17dc2669d938332ff2
Applesry <129361139+Applestry@users.noreply.github.com>
2023-07-28 16:21:07 +0700

    Update Main.cpp

--- Lab3/Main.cpp
+++ Lab3/Main.cpp
@@ -10,36 +10,35 @@ using namespace std;
 //}
 
 int main(int argc, char* argv[]) {
-	if (argc < 2)
-		return 0;
-	if (strcmp(argv[1], "-a") == 0) {
+	string mode = argv[1];
+	if (mode == "-a") {
 		if (argc == 5) {
-			//command 1 or 3
-			Command_1(argc, argv);
+			if ('0' < argv[3][0] && argv[3][0] <= '9')
+			Command_3(argc, argv);
+			else Command_1(argc, argv);
 		}
 		else if (argc == 6) {
 			//command 2
 			Command_2(argc, argv);
 		}
 		else {
-			cout << "invalid command line argument" << endl;
+			cout << "Insufficient command line arguments!" << endl;
 		}
 	}
-	else if (strcmp(argv[1], "-c") == 0) {
+	else if (mode == "-c") {
 		if (argc == 5) {
 			//command 4
 			Command_4(argc, argv);
 		}
 		else if (argc == 6) {
 			//command 5
-			Command_5(argc, argv);
 		}
 		else {
-			cout << "invalid command line argument" << endl;
+			cout << "Insufficient command line arguments!" << endl;
 		}
 	}
 	else {
-		cout << "invalid command line argument" << endl;
+		cout << "Invalid command line argument" << endl;
 	}
 	return 0;
 }
