064897bf1636f6d91466c89b273a2f9900e28104
Applesry <129361139+Applestry@users.noreply.github.com>
2023-07-29 13:49:05 +0700

    Update Command.cpp

--- Lab3/Command.cpp
+++ Lab3/Command.cpp
@@ -1,61 +1,62 @@
 #include "Command.h"
+
 using namespace std;
 
-bool check_algorithm(char algorithm[], int a[], int n, long long& count_compare, double& running_time) {
+bool check_algorithm(string algorithm, int a[], int n, long long& count_compare, double& running_time) {
 	count_compare = 0;
 	running_time = 0;
 	clock_t start, end;
-	if (strcmp(algorithm, "selection-sort") == 0) {
+	if (algorithm == "selection-sort") {
 		start = clock();
 		selection_sort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "insertion-sort") == 0) {
+	else if (algorithm == "insertion-sort") {
 		start = clock();
 		insertionSort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "bubble-sort") == 0) {
+	else if (algorithm == "bubble-sort") {
 		start = clock();
 		bubbleSort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "shaker-sort") == 0) {
+	else if (algorithm == "shaker-sort") {
 		start = clock();
 		shakerSort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "shell-sort") == 0) {
+	else if (algorithm == "shell-sort") {
 		start = clock();
 		shell_sort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "heap-sort") == 0) {
+	else if (algorithm == "heap-sort") {
 		start = clock();
 		heapSort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "merge-sort") == 0) {
+	else if (algorithm == "merge-sort") {
 		start = clock();
 		mergeSort(a, 0, n - 1, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "quick-sort") == 0) {
+	else if (algorithm == "quick-sort") {
 		start = clock();
 		quick_sort(a, 0, n - 1, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "counting-sort") == 0) {
+	else if (algorithm == "counting-sort") {
 		start = clock();
-		countingSort(a, n, count_compare);
+		counting_sort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "radix-sort") == 0) {
+	else if (algorithm == "radix-sort") {
 		start = clock();
 		radixSort(a, n, count_compare);
 		end = clock();
 	}
-	else if (strcmp(algorithm, "flash-sort") == 0) {
+	else if (algorithm == "flash-sort") {
 		start = clock();
 		flashSort(a, n, count_compare);
 		end = clock();
@@ -68,24 +69,24 @@ bool check_algorithm(char algorithm[], int a[], int n, long long& count_compare,
 	return 1;
 }
 
-int check_inputOrder(char s[]) {
-	if (strcmp(s, "-rand") == 0)
+int check_inputOrder(string s) {
+	if (s == "-rand")
 		return 0;
-	else if (strcmp(s, "-sorted") == 0)
+	else if (s == "-sorted")
 		return 1;
-	else if (strcmp(s, "-rev") == 0)
+	else if (s == "-rev")
 		return 2;
-	else if (strcmp(s, "-nsorted") == 0)
+	else if (s == "-nsorted")
 		return 3;
 	return -1;
 }
 
-int check_outputParameter(char s[]) {
-	if (strcmp(s, "-time") == 0)
+int check_outputParameter(string s) {
+	if (s == "-time")
 		return 0;
-	else if (strcmp(s, "-comp") == 0)
+	else if (s == "-comp")
 		return 1;
-	else if (strcmp(s, "-both") == 0)
+	else if (s == "-both")
 		return 2;
 	return -1;
 }
@@ -134,18 +135,21 @@ void Command_1(int argc, char* argv[]) {
 }
 
 void Command_2(int argc, char* argv[]) {
-	int check_1 = check_outputParameter(argv[5]);
+	string outpPar = argv[5];
+	int check_1 = check_outputParameter(outpPar);
 	if (check_1 == -1) {
 		cout << "Invalid output parameter" << endl;
 		return;
 	}
-	int n = atoi(argv[3]);
+	string inpSize = argv[3];
+	int n = stoi(inpSize);
 	if (n < 0) {
 		cout << "Invalid input size" << endl;
 		return;
 	}
 	int* a = new int[n];
-	int check_2 = check_inputOrder(argv[4]);
+	string inpOrder = argv[4];
+	int check_2 = check_inputOrder(inpOrder);
 	if (check_2 == -1) {
 		cout << "Invalid input order" << endl;
 		return;
@@ -153,28 +157,15 @@ void Command_2(int argc, char* argv[]) {
 	else {
 		GenerateData(a, n, check_2);
 	}
-	ofstream f1;
-	f1.open("input.txt", ios_base::out);
-	f1 << n << endl;
-	for (int i = 0; i < n; i++) {
-		f1 << a[i] << " ";
-	}
-	f1.close();
+	string algorithm = argv[2];
 	double running_time = 0;
 	long long count_compare = 0;
-	if (!check_algorithm(argv[2], a, n, count_compare, running_time)) {
+	if (!check_algorithm(algorithm, a, n, count_compare, running_time)) {
 		cout << "Invalid algorithm" << endl;
 		return;
 	}
-	ofstream f2;
-	f2.open("output.txt", ios_base::out);
-	f2 << n << endl;
-	for (int i = 0; i < n; i++) {
-		f2 << a[i] << " ";
-	}
-	f2.close();
 	cout << "ALGORITHM MODE" << endl;
-	cout << "Algorithm:" << argv[2] << endl;
+	cout << "Algorithm:" << algorithm << endl;
 	cout << "Input size:" << n << endl;
 	cout << "Input order:";
 	switch (check_2) {
@@ -205,6 +196,7 @@ void Command_2(int argc, char* argv[]) {
 		break;
 	}
 }
+
 void Command_3(int argc, char* argv[]) {
 
 	double running_time = 0;
@@ -357,7 +349,6 @@ void Command_3(int argc, char* argv[]) {
 	delete[] a;
 }
 
-
 void Command_4(int argc, char* argv[]) {
 	string algorithm_1 = argv[2];
 	string algorithm_2 = argv[3];
@@ -376,11 +367,15 @@ void Command_4(int argc, char* argv[]) {
 	double running_time_2 = 0;
 	long long count_compare_1 = 0;
 	long long count_compare_2 = 0;
-	if (!check_algorithm(algorithm_1, a, n, count_compare_1, running_time_1) || !check_algorithm(algorithm_2, a, n, count_compare_2, running_time_2)) {
+	if (!check_algorithm(algorithm_1, a, n, count_compare_1, running_time_1)) {
 		cout << "Invalid algorithm specified!" << endl;
 		return;
 	}
-	cout << "COMPARE MODE" << endl;
+	if (!check_algorithm(algorithm_2, a, n, count_compare_2, running_time_2)) {
+		cout << "Invalid algorithm specified!" << endl;
+		return;
+	}
+	cout << "ALGORITHM MODE" << endl;
 	cout << "Algorithm:" << algorithm_1 << " | " << algorithm_2 << endl;
 	cout << "Input file:" << inputFilename << endl;
 	cout << "Input size:" << n << endl;
